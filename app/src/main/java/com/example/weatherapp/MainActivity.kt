13ee406503dd83e3c3ba061c0d5f596756c3f66e
Aleksansdr <sashaspj@gmail.com>
2023-09-01 21:06:49 +0100

    Add binding, Pager, Teb

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,31 +3,35 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
+import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.network.RetrofitHelper
 import com.example.weatherapp.network.WeatherApi
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var locationLable : TextView
-    private lateinit var currentWeatherLable : TextView
-    private lateinit var forecastLable : TextView
+    private lateinit var viewPager : ViewPager2
+    private lateinit var tabLayout : TabLayout
 
     private val addId = "45d5bc00fe8f4f8564a6a54bd8a1b85c"
-
+    //type safety,null safety,improved performance,and better readability
+    private lateinit var binding : ActivityMainBinding
 
     private val retrofitClient = RetrofitHelper.getInstance().create(WeatherApi::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //ИНИЦИАЛИЗИРУЕМ БИНДИНГ
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        locationLable = findViewById(R.id.locationLable)
-        currentWeatherLable = findViewById(R.id.currentWeatherLable)
-        forecastLable = findViewById(R.id.forecastLable)
+        viewPager = binding.viewPager
+        tabLayout = binding.tabLayout
 
         lifecycleScope.launch(Dispatchers.IO){
             val result = retrofitClient.getGeocoding("London",limit = "1",addId)
@@ -44,11 +48,27 @@ class MainActivity : AppCompatActivity() {
             Log.d("testlog","Forecast-------> ${forecast.body()}")
 
             withContext(Dispatchers.Main){
-                locationLable.text = "Location: $latResult, $lonResult"
-                currentWeatherLable.text = currentWeather.body()?.weather?.first()?.description ?: ""
-                forecastLable.text = forecast.body()?.list?.first()?.weather?.first()?.description ?: ""
+//                locationLable.text = "Location: $latResult, $lonResult"
+//                currentWeatherLable.text = currentWeather.body()?.weather?.first()?.description ?: ""
+//                forecastLable.text = forecast.body()?.list?.first()?.weather?.first()?.description ?: ""
             }
         }
 
+        prepareViewPager()
+
+    }
+
+    private fun prepareViewPager() {
+        val fragmentList = arrayListOf(
+            WeatherFragment.newInstance(),
+            ForecastFragment.newInstance()
+        )
+        val tabTitlesArray = arrayOf("Weather","Forecast")
+
+        viewPager.adapter = ViewPagerAdapter(this, fragmentList)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitlesArray[position]
+        }.attach()
     }
 }
