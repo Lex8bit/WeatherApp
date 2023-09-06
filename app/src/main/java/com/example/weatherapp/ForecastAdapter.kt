874da235c3b2d72d5d1bb55fb40799828893bd23
Aleksansdr <sashaspj@gmail.com>
2023-09-06 21:38:38 +0100

    Починил forecast.Данные отображаются корректро. ВСЕ РАБОТАЕТ

--- app/src/main/java/com/example/weatherapp/ForecastAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ForecastAdapter.kt
@@ -3,10 +3,15 @@ package com.example.weatherapp
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.databinding.ItemForecastBinding
 import com.example.weatherapp.model.forecast.ForecastResult
+import com.example.weatherapp.repository.WeatherRepository
+import com.example.weatherapp.repository.WeatherRepositoryImpl
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class ForecastAdapter(
     private val fragmentContext: Context,
@@ -16,8 +21,72 @@ class ForecastAdapter(
     class ViewHolder(private val binding: ItemForecastBinding, private val context: Context) :
         RecyclerView.ViewHolder(binding.root) {
         fun bindItem(forecast: ForecastResult){
-            binding.itemRecyclerTemp.text = "Temperature: ${forecast.temp}"
+            val inputDataFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val outputDataFormat = SimpleDateFormat("d MMMM HH:mm", Locale.getDefault())
+
+            val date = inputDataFormat.parse(forecast.date)
+            val outputDate = date?.let { outputDataFormat.format(it) }
+            binding.itemRecyclerDate.text = "Date: $outputDate"
+            binding.itemRecyclerTemp.text = "Temp: ${forecast.temp}"
             binding.itemRecyclerDescription.text = "Weather: ${forecast.descriptor}"
+            when(forecast.main){
+                WeatherRepositoryImpl.WEATHER_TYPE_CLEAR -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.clear_sky
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_CLOUDS -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.clouds
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_SNOW -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.snow
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_THUNDERSTORM -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.thunderstorm
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_RAIN -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.heavy_rain
+                        )
+                    )
+                }
+                WeatherRepositoryImpl.WEATHER_TYPE_CLEAR -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.clear_sky
+                        )
+                    )
+                }
+                else -> {
+                    binding.itemRecyclerImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.mist
+                        )
+                    )
+                }
+            }
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastAdapter.ViewHolder {
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.weatherapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -10,6 +12,7 @@ import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.viewmodel.MainViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,7 +24,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager : ViewPager2
     private lateinit var tabLayout : TabLayout
+    private lateinit var inputText : TextInputLayout
+
     private lateinit var binding : ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +37,20 @@ class MainActivity : AppCompatActivity() {
 
         viewPager = binding.viewPager
         tabLayout = binding.tabLayout
+        inputText = binding.mainInputField
 
-        lifecycleScope.launch(Dispatchers.Main){
-            mainViewModel.getCoordinates("London")
+        inputText.setEndIconOnClickListener{
+            /**
+             * Закрываем клавиатуру после того как ввели название города
+             */
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            if (inputMethodManager.isActive){
+                inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken,0)
+            }
+
+            lifecycleScope.launch(Dispatchers.Main){
+                mainViewModel.getCoordinates(inputText.editText?.text.toString())
+            }
         }
 
         mainViewModel.coordinatesResult.observe(this, Observer {
